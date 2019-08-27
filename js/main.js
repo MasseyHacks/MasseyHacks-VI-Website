b74942273c261b24622690fe10cba4e8ab13bb9e
David Hui <opensource@davidhui.ca>
2019-08-26 22:30:45 -0400

    code cleanup

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
-let faq = {
-  currentIndex: 0,
-  available: [["What is the admission cost?", "Admission to MasseyHacks VI is $25. If you require a subsidy, please email us at <a href=\"mailto:hello@masseyhacks.ca\">hello@masseyhacks.ca</a>."], ["Is Apple any good?","Apple products are inferior to all other products on the market due to their extremely low quality material and stuck-up way of treating their customers"], ["This is a very long question that does not have any true meaning other than the fact that it is an extremely long question.", "hlello!"]]
-};
-
 let countUpI;
 
 function countUp() {
