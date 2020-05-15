efab57e6a2f469a6091c98f0ecacd4316bf5fb35
James Xu <xujames007@gmail.com>
2020-05-15 16:27:32 -0400

    added youtube stream integration

--- live/js/stream-gallery.js
+++ live/js/stream-gallery.js
@@ -1,12 +1,20 @@
 function myFunction(imgs) {
     // Get the expanded image
-    let expandImg = document.getElementById("expandedImg");
+    // let expandImg = document.getElementById("expandedImg");
     // Get the image text
-    let imgText = document.getElementById("imgtext");
+    let expandImg = $("#expandedImage")
+    let imgText = $("#imgtext")
     // Use the same src in the expanded image as the image being clicked on from the grid
-    expandImg.src = imgs.src;
+    expandImg.attr('src', "https://www.youtube-nocookie.com/embed/" + imgs.dataset.youtubeid)
+    // console.log(imgs.dataset.streamurl)
     // Use the value of the alt attribute of the clickable image as text inside the expanded image
     imgText.innerHTML = imgs.alt;
     // Show the container element (hidden with CSS)
-    expandImg.parentElement.style.display = "block";
+    // expandImg.parentElement.style.display = "block";
 }
+
+$(() => {
+    $(".stream-thumbnail").each(function (index) {
+        $(this).attr('src', `https://img.youtube.com/vi/${$(this).data('youtubeid')}/maxresdefault.jpg`)
+    })
+})
