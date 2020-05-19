c4b0e074a5fd4e58ae813c543b3f0fbcbfc4e691
David Hui <opensource@davidhui.ca>
2020-05-19 15:15:57 -0400

    pull videos from JSON

--- live/js/stream-gallery.js
+++ live/js/stream-gallery.js
@@ -5,7 +5,7 @@ function myFunction(imgs) {
     let expandImg = $("#expandedImage")
     let imgText = $("#imgtext")
     // Use the same src in the expanded image as the image being clicked on from the grid
-    expandImg.attr('src', `https://www.youtube.com/embed/${imgs.dataset.youtubeid}?&autoplay=1`)
+    expandImg.attr('src', `https://www.youtube.com/embed/${$(imgs).data('youtubeid')}?&autoplay=1`)
     // console.log(imgs.dataset.streamurl)
     // Use the value of the alt attribute of the clickable image as text inside the expanded image
     imgText.html(imgs.alt);
@@ -19,8 +19,25 @@ function myFunction(imgs) {
     // expandImg.parentElement.style.display = "block";
 }
 
+function getIDFromURL(url){
+    return url.substring(url.lastIndexOf("/")+1);
+}
+
 $(() => {
-    $(".stream-thumbnail").each(function (index) {
-        $(this).attr('src', `https://img.youtube.com/vi/${$(this).data('youtubeid')}/maxresdefault.jpg`)
+    $.get({
+        url: "https://eventdata.masseyhacks.ca/streams.json",
+        dataType: "json",
+        cache: false,
+        success: function(data){
+            $("#expandedImage").attr('src', data["general"]);
+            $("#thumb-general").data('youtubeid', getIDFromURL(data["general"]));
+            $("#thumb-yellow").data('youtubeid', getIDFromURL(data["yellow"]));
+            $("#thumb-green").data('youtubeid', getIDFromURL(data["green"]));
+            $("#thumb-magenta").data('youtubeid', getIDFromURL(data["magenta"]));
+
+            $(".stream-thumbnail").each(function (index) {
+                $(this).attr('src', `https://img.youtube.com/vi/${$(this).data('youtubeid')}/maxresdefault.jpg`)
+            });
+        }
     })
 })
